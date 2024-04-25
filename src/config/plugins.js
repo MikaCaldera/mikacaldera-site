e2f3ce369214cb54c89bbba9498401a3b782f2e2
GarrettWeinberg <weinberg135@gmail.com>
2024-04-25 12:56:32 -0400

    Add @rollup/plugin-terser to package.json and update eleventy-plugin-rollup configuration in src/config/plugins.js

--- src/config/plugins.js
+++ src/config/plugins.js
@@ -1,7 +1,7 @@
 /**
  * Add Eleventy plugins here
  * https://www.11ty.dev/docs/plugins/
-*/
+ */
 
 module.exports = {
   /**
@@ -10,7 +10,7 @@ module.exports = {
    */
   metagen: function (eleventyConfig) {
     // Require dependencies
-    let plugin = require('eleventy-plugin-metagen');
+    let plugin = require("eleventy-plugin-metagen");
 
     // Add plugin to eleventyConfig
     eleventyConfig.addPlugin(plugin);
@@ -20,9 +20,9 @@ module.exports = {
    * Rollup plugin to bundle JavaScript
    * https://github.com/Snapstromegon/eleventy-plugin-rollup
    */
-   rollup: function(eleventyConfig) {
-    let plugin = require('eleventy-plugin-rollup');
-    let { terser } = require("rollup-plugin-terser");
+  rollup: function (eleventyConfig) {
+    let plugin = require("eleventy-plugin-rollup");
+    let terser = require("@rollup/plugin-terser");
     let { nodeResolve } = require("@rollup/plugin-node-resolve");
     let replace = require("@rollup/plugin-replace");
     let commonjs = require("@rollup/plugin-commonjs");
@@ -35,23 +35,23 @@ module.exports = {
         output: {
           format: "esm",
           sourcemap: true,
-          dir: 'public/assets/scripts'
+          dir: "public/assets/scripts",
         },
 
         // Configure the plugins
         plugins: [
           replace({
             preventAssignment: true,
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+            "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
           }),
           nodeResolve(),
           terser(),
           commonjs(),
         ],
-      }
+      },
     };
 
     // Add the plugin to the Eleventy config
-    eleventyConfig.addPlugin(plugin, config)
+    eleventyConfig.addPlugin(plugin, config);
   },
-}
+};
